a8b1fff76c1e2601a085775fa8fc29ce90489030
Guilherme <guilherme.llopreti@gmail.com>
2022-11-18 15:06:50 -0300

    feat: create user validator

--- src/routes/users/index.ts
+++ src/routes/users/index.ts
@@ -1,8 +1,16 @@
 import { Router } from "express";
+import { expressYupMiddleware } from "express-yup-middleware";
+import createUserValidator from "../../validations/users";
+
 import UserController from "../../controllers/user.controller";
 
 const usersRouter = Router();
 
-usersRouter.route("").post(UserController.store);
+usersRouter
+  .route("")
+  .post(
+    expressYupMiddleware({ schemaValidator: createUserValidator }),
+    UserController.store
+  );
 
 export default usersRouter;
---
+++ src/validations/users/index.ts
@@ -0,0 +1,23 @@
+import * as yup from "yup";
+
+const createUserValidator = {
+  schema: {
+    body: {
+      yupSchema: yup.object().shape({
+        username: yup
+          .string()
+          .required("username is required")
+          .min(3, "minimum 3 characters"),
+        password: yup
+          .string()
+          .required("password is required")
+          .matches(
+            /^(?=.*[A-Z])(?=.*[0-9])[0-9a-zA-Z$*&@#]{8,}$/,
+            "password must have at least 8 characters, 1 uppercase letter and 1 number"
+          ),
+      }),
+    },
+  },
+};
+
+export default createUserValidator;
